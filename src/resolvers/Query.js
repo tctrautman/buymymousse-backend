9e0d9fe3d1c8d04fd5ba60ada4b082d97a1f844d
tctrautman <tctrautman@gmail.com>
2019-03-28 11:20:48 -0700

    Remove old comments

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,13 +3,6 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
     items: forwardTo('db'),
     item: forwardTo('db')
-    // async items(parent, args, ctx, info) {
-    //     const items = await ctx.db.query.items()
-
-    //     console.log(items)
-
-    //     return items;
-    // }
 };
 
 module.exports = Query;
