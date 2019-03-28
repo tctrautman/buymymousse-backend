ef607fc773a4072c4019bc43d576e9226dc988e9
tctrautman <tctrautman@gmail.com>
2019-03-28 14:26:02 -0700

    Add Mutation and schmea update to allow user to delte an item

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -26,6 +26,15 @@ const Mutations = {
         },
         info
         );
+    },
+    async deleteItem(parent, args, ctx, info) {
+        const where =  { id: args.id };
+        // Find the item
+        const item = await ctx.db.query.item({ where }, `{ id title }`);
+        // Check if they own that item or have permissions
+        // TODO
+        // if so, delete it
+        return ctx.db.mutation.deleteItem({ where }, info)
     }
 };
 
