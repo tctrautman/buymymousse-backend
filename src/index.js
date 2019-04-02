e868c0a7b2eaad5962780ba6a82bfc36c3995de8
Tim Trautman <tctrautman@gmail.com>
2019-04-02 14:00:58 -0700

    Add query for checking if a user is signed in (#7)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const cookieParser = require('cookie-parser');
-
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: '.env'});
 const createServer = require('./createServer');
 const db = require('./db');
@@ -7,7 +7,18 @@ const db = require('./db');
 const server = createServer();
 
 server.express.use(cookieParser());
-// To do: us express middleware to populate current user
+
+// Decode the JWT so we can get the user ID on each request
+server.express.use((req, res, next) => {
+    const {token} = req.cookies;
+    
+    if (token) {
+        const {userId} = jwt.verify(token, process.env.APP_SECRET);
+        // Put the userId onto the req for future request access
+        req.userId = userId;
+    }
+    next();
+});
 
 server.start({
     cors: {
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,7 +3,16 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
     items: forwardTo('db'),
     item: forwardTo('db'),
-    itemsConnection: forwardTo('db')
+    itemsConnection: forwardTo('db'),
+    me(parent, args, ctx, info) {
+        // Check if there is a current userId
+        if (!ctx.request.userId) {
+            return null;
+        }
+        return ctx.db.query.user({
+            where: {id: ctx.request.userId},
+        }, info);
+    }
 };
 
 module.exports = Query;
