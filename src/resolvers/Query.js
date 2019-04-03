f92fd6ee8b92da9a086eef4d9619a27611391986
tctrautman <tctrautman@gmail.com>
2019-04-02 22:14:25 -0700

    Handle permission changes from admin page

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
 
 const Query = {
     items: forwardTo('db'),
@@ -12,6 +13,18 @@ const Query = {
         return ctx.db.query.user({
             where: {id: ctx.request.userId},
         }, info);
+    },
+    async users(parent, args, ctx, info) {
+        // 1. Check if they're logged in
+        if (!ctx.request.userId) {
+            throw new Error('You must be logged in!');
+        }
+        
+        // 2. Check if the user has the permissions to query all users
+        hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+        
+        // 3. if they do, query all the users
+        return ctx.db.query.users({}, info);
     }
 };
 
